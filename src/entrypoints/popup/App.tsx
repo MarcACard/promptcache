d5d8ec39f0c407f0bab105801c0235e204cb93bc
Marc A Card <marcacard@gmail.com>
2025-01-20 02:26:17 -0500

    Setup - tailwindcss & shadcn components

--- src/entrypoints/popup/App.tsx
+++ src/entrypoints/popup/App.tsx
@@ -1,22 +1,17 @@
 import { useState } from 'react';
 import reactLogo from '@/assets/react.svg';
-import wxtLogo from '/wxt.svg';
-import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://wxt.dev" target="_blank">
-          <img src={wxtLogo} className="logo" alt="WXT logo" />
-        </a>
+      <div className="min-w-[400px]">
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>WXT + React</h1>
+      <h1 className="text-3xl font-bold underline">WXT + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
